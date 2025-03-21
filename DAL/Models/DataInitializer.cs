a0e29e2edfe8fd6bf43cf071230e98d097e4c7a6
Nelly <Nelly.sosobrado@gmail.com>
2025-03-21 11:14:06 +0100

    Created DataInitializer.cs, class to seed data to database

---
+++ DAL/Models/DataInitializer.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Models
+{
+    public class DataInitializer //This class is used to seed the database with initial data
+    {
+        private readonly BankAppDataContext _dbContext;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public DataInitializer(BankAppDataContext dbContext, UserManager<IdentityUser> userManager)
+        {
+            _dbContext = dbContext;
+            _userManager = userManager;
+        }
+        public void SeedData()
+        {
+            _dbContext.Database.Migrate();
+            SeedRoles();
+            SeedUsers();
+        }
+
+
+        //Here you can update your user login credentials
+        private void SeedUsers()
+        {
+            AddUserIfNotExists("richard.chalk@systementor.se", "Hejsan123#", new string[] { "Admin" });
+            AddUserIfNotExists("richard.chalk@customer.systementor.se", "Hejsan123#", new string[] { "Cashier" });
+        }
+
+        //Here you can update your user roles
+        private void SeedRoles()
+        {
+            AddRoleIfNotExisting("Admin");
+            AddRoleIfNotExisting("Cashier");
+        }
+
+        private void AddRoleIfNotExisting(string roleName)
+        {
+            var role = _dbContext.Roles.FirstOrDefault(r => r.Name == roleName);
+            if (role == null)
+            {
+                _dbContext.Roles.Add(new IdentityRole { Name = roleName, NormalizedName = roleName });
+                _dbContext.SaveChanges();
+            }
+        }
+
+        private void AddUserIfNotExists(string userName, string password, string[] roles)
+        {
+            if (_userManager.FindByEmailAsync(userName).Result != null) return;
+
+            var user = new IdentityUser
+            {
+                UserName = userName,
+                Email = userName,
+                EmailConfirmed = true
+            };
+            _userManager.CreateAsync(user, password).Wait();
+            _userManager.AddToRolesAsync(user, roles).Wait();
+        }
+    }
+}
