33009b55f453f9436a2d01c36d2732648bdcfa62
Nelly <Nelly.sosobrado@gmail.com>
2025-03-21 18:22:14 +0100

    Created UserRole property

--- BankApplication/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ BankApplication/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -70,6 +70,11 @@ namespace BankApplication.Areas.Identity.Pages.Account
         /// </summary>
         public class InputModel
         {
+            [DataType(DataType.Text)]
+            [Display(Name = "User role")]
+            public string UserRole { get; set; }
+
+
             /// <summary>
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
