17e3e245c77593e08b9d1e3e345ebdcb080eb39f
Nelly <Nelly.sosobrado@gmail.com>
2025-03-26 11:53:02 +0100

    Fixed Login.cshtml not displaying

--- BankApplication/Program.cs
+++ BankApplication/Program.cs
@@ -5,24 +5,28 @@ using DAL.Models;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
+    ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
 builder.Services.AddDbContext<BankAppDataContext>(options =>
     options.UseSqlServer(connectionString));
+
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddRoles<IdentityRole>() //Implements Role based authorization
+    .AddRoles<IdentityRole>() // Implements Role-based authorization
     .AddEntityFrameworkStores<BankAppDataContext>();
-builder.Services.AddRazorPages();
 
+builder.Services.AddRazorPages();
 builder.Services.AddTransient<DataInitializer>();
+
 var app = builder.Build();
+
 
 using (var scope = app.Services.CreateScope())
 {
     scope.ServiceProvider.GetService<DataInitializer>().SeedData();
 }
-
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -32,17 +36,19 @@ if (app.Environment.IsDevelopment())
 else
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    app.UseHsts(); // HTTP Strict Transport Security
 }
 
 app.UseHttpsRedirection();
+app.UseStaticFiles();
 
 app.UseRouting();
 
+
+app.UseAuthentication(); 
 app.UseAuthorization();
 
-app.MapStaticAssets();
+app.MapStaticAssets(); 
 app.MapRazorPages()
    .WithStaticAssets();
 
