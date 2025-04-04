c8dbf08120431ecdeaf8408429c27e147634875e
Nelly <Nelly.sosobrado@gmail.com>
2025-04-04 17:08:18 +0200

    Withdraw function, Displaying balance at the same time with error messages when theres no input

--- BankApplication/Pages/Account/Withdraw.cshtml.cs
+++ BankApplication/Pages/Account/Withdraw.cshtml.cs
@@ -1,9 +1,11 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Services.Interface;
+using System.ComponentModel.DataAnnotations;
 
 namespace BankApplication.Pages.Account
 {
+    [BindProperties]
     public class WithdrawModel : PageModel
     {
         private readonly IAccountService _accountService;
@@ -13,9 +15,34 @@ namespace BankApplication.Pages.Account
             _accountService = accountService;
         }
 
-        public decimal Amount { get;set; }
-        public void OnGet()
+        [Range(100, 10000)]
+        public decimal Amount { get; set; }
+
+        public decimal Balance { get; set; }
+        public void OnGet(int accountId)
         {
+            var accountDb = _accountService.GetAccount(accountId);
+            Balance = accountDb.Balance;
+
+            // Option 2 - Cleaner?
+            // Balance = _accountService.GetAccount(accountId).Balance;
+        }
+
+        public IActionResult OnPost(int accountId)
+        {
+            var accountDb = _accountService.GetAccount(accountId);
+            if (accountDb.Balance < Amount)
+            {
+                ModelState.AddModelError("Amount", "You don't have that much money!");
+            }
+
+            if (ModelState.IsValid)
+            {
+                accountDb.Balance -= Amount;
+                _accountService.Update(accountDb);
+                return RedirectToPage("Index");
+            }
+            return Page();
         }
     }
 }
