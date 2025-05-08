cd0c819312599657721f39a17aff553f4776d6be
Nelly <Nelly.sosobrado@gmail.com>
2025-05-08 20:03:50 +0200

    Updated Transaction vye, added and reused paginationlist

--- BankApplication/Pages/AccountTransactions.cshtml.cs
+++ BankApplication/Pages/AccountTransactions.cshtml.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using BankApplication.ViewModels;
 using Services.ViewModels;
+using Services;
 
 namespace BankApplication.Pages
 {
@@ -49,31 +50,53 @@ namespace BankApplication.Pages
 
             return Page();
         }
-
-
-
-
-
-
-
-        public async Task<IActionResult> OnGetLoadMoreAsync(int accountId, int skip)
+        public async Task<IActionResult> OnGetLoadMore(int accountId, int skip)
         {
-            var transactions = await _bankAppDataContext.Transactions
-                .Where(t => t.AccountId == accountId)
-                .OrderByDescending(t => t.Date)
-                .Skip(skip)
-                .Take(20)
-                .Select(t => new TransactionViewModel
-                {
-                    TransactionId = t.TransactionId,
-                    Type = t.Type,
-                    Operation = t.Operation,
-                    Amount = t.Amount,
-                    Balance = t.Balance
-                })
-                .ToListAsync();  
+            const int pageSize = 20;
 
-            return new JsonResult(transactions);
+            var query = _bankAppDataContext.Transactions
+                .Where(t => t.AccountId == accountId)
+                .OrderByDescending(t => t.Date);
+
+            var totalCount = await query.CountAsync();
+            var transactions = await query.Skip(skip).Take(pageSize).ToListAsync();
+
+            var paginatedTransactions = new PaginatedList<Transaction>(
+                transactions,
+                totalCount,
+                skip / pageSize + 1, // PageIndex, beräknat från skip
+                pageSize
+            );
+
+            // Skicka tillbaka både transaktionerna och information om det finns fler sidor
+            return new JsonResult(new { transactions = paginatedTransactions.Items, hasMore = paginatedTransactions.HasNextPage });
         }
+
+
+
+
+
+
+
+
+        //public async Task<IActionResult> OnGetLoadMoreAsync(int accountId, int skip)
+        //{
+        //    var transactions = await _bankAppDataContext.Transactions
+        //        .Where(t => t.AccountId == accountId)
+        //        .OrderByDescending(t => t.Date)
+        //        .Skip(skip)
+        //        .Take(20)
+        //        .Select(t => new TransactionViewModel
+        //        {
+        //            TransactionId = t.TransactionId,
+        //            Type = t.Type,
+        //            Operation = t.Operation,
+        //            Amount = t.Amount,
+        //            Balance = t.Balance
+        //        })
+        //        .ToListAsync();  
+
+        //    return new JsonResult(transactions);
+        //}
     }
 }
