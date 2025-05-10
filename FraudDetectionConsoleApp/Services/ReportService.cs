dcb6120843d86c0af63c3e440758f99155cfa871
Nelly <Nelly.sosobrado@gmail.com>
2025-05-10 20:28:16 +0200

    cleanup

--- FraudDetectionConsoleApp/Services/ReportService.cs
+++ FraudDetectionConsoleApp/Services/ReportService.cs
@@ -11,23 +11,21 @@ namespace FraudDetectionConsoleApp.Services
 
         public ReportService()
         {
-            // Hitta den absoluta sökvägen till Reports-mappen i BankApplication\FraudDetectionConsoleApp
+
             _reportFolderPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\Reports");
 
-            // Kontrollera om mappen finns, annars skapa den
             if (!Directory.Exists(_reportFolderPath))
             {
                 Directory.CreateDirectory(_reportFolderPath);
             }
         }
 
-        // Asynkron metod för att spara rapporten
         public async Task SaveReportAsync(string countryCode, List<string> report)
         {
-            // Skapa rapportfilens sökväg baserat på landkoden och datumet
+
             string reportFilePath = Path.Combine(_reportFolderPath, $"report_{countryCode}_{DateTime.Now:yyyyMMdd_HHmmss}.txt");
 
-            // Spara rapporten asynkront
+
             await File.WriteAllLinesAsync(reportFilePath, report);
 
             Console.WriteLine($"Rapport för {countryCode} sparad på: {reportFilePath}");
