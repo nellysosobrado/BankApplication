a5770db65cf93e2512466f3b5edffe04d15216b7
Nelly <Nelly.sosobrado@gmail.com>
2025-05-11 11:19:01 +0200

    Created new class CustomerSorter.cs.
    Fixed Sort on customer list button not sorting

--- Services/Interface/ICustomerSorter.cs
+++ Services/Interface/ICustomerSorter.cs
@@ -12,22 +12,6 @@ namespace Services.Interface
         IQueryable<Customer> ApplySorting(IQueryable<Customer> query, string sortColumn, string sortOrder);
     }
 
-    public class CustomerSorter : ICustomerSorter
-    {
-        public IQueryable<Customer> ApplySorting(IQueryable<Customer> query, string column, string order)
-        {
-            bool asc = order?.ToLower() == "asc";
-
-            return column switch
-            {
-                "CustomerId" => asc ? query.OrderBy(c => c.CustomerId) : query.OrderByDescending(c => c.CustomerId),
-                "Personnummer" => asc ? query.OrderBy(c => c.NationalId) : query.OrderByDescending(c => c.NationalId),
-                "Name" => asc ? query.OrderBy(c => c.Surname) : query.OrderByDescending(c => c.Surname),
-                "Address" => asc ? query.OrderBy(c => c.Streetaddress) : query.OrderByDescending(c => c.Streetaddress),
-                "City" => asc ? query.OrderBy(c => c.City) : query.OrderByDescending(c => c.City),
-                _ => query.OrderBy(c => c.Surname)
-            };
-        }
-    }
+    
 
 }
---
+++ Services/Services/CustomerSorter.cs
@@ -0,0 +1,31 @@
+﻿using DAL.Models;
+using Services.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Services
+{
+    public class CustomerSorter : ICustomerSorter
+    {
+        public IQueryable<Customer> ApplySorting(IQueryable<Customer> query, string column, string order)
+        {
+            bool asc = order?.ToLower() == "asc";
+
+            return column switch
+            {
+                "CustomerId" => asc ? query.OrderBy(c => c.CustomerId) : query.OrderByDescending(c => c.CustomerId),
+                "Personnummer" => asc ? query.OrderBy(c => c.NationalId) : query.OrderByDescending(c => c.NationalId),
+                "Name" => asc
+                    ? query.OrderBy(c => c.Givenname).ThenBy(c => c.Surname)
+                    : query.OrderByDescending(c => c.Givenname).ThenByDescending(c => c.Surname),
+                "Address" => asc ? query.OrderBy(c => c.Streetaddress) : query.OrderByDescending(c => c.Streetaddress),
+                "City" => asc ? query.OrderBy(c => c.City) : query.OrderByDescending(c => c.City),
+                _ => query.OrderBy(c => c.Surname)
+            };
+        }
+
+    }
+}
