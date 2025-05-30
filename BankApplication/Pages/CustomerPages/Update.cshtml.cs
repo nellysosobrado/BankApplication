f5add8838bc0090b5f2362339b6610ff0d17b2a4
Nelly <Nelly.sosobrado@gmail.com>
2025-05-30 12:37:55 +0200

    Updated Update to only use PRG, updated error messages when filling in update form

--- BankApplication/Pages/CustomerPages/Update.cshtml.cs
+++ BankApplication/Pages/CustomerPages/Update.cshtml.cs
@@ -4,9 +4,11 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Services;
 using Services.Interface;
 using Services.ViewModels;
-
+using System.Linq;
+using System.Text.Json;
 
 namespace BankApplication.Pages.CustomerPages;
+
 [Authorize(Roles = "Cashier")]
 public class UpdateModel : PageModel
 {
@@ -20,28 +22,61 @@ public class UpdateModel : PageModel
     [BindProperty]
     public CustomerInputModel Input { get; set; }
 
+    [TempData]
+    public string? TempInputJson { get; set; }
+
+    [TempData]
+    public string? TempModelStateJson { get; set; }
+
     public IActionResult OnGet(int id)
     {
-        var customer = _commandService.GetCustomer(id);
-        if (customer == null) return NotFound();
+   
+        Response.Headers["Cache-Control"] = "no-store, no-cache, must-revalidate, max-age=0";
+        Response.Headers["Pragma"] = "no-cache";
+        Response.Headers["Expires"] = "0";
 
-        Input = new CustomerInputModel
+        if (!string.IsNullOrEmpty(TempInputJson))
         {
-            CustomerId = customer.CustomerId,
-            Givenname = customer.Givenname,
-            Surname = customer.Surname,
-            Gender = customer.Gender,
-            Birthday = customer.Birthday,
-            City = customer.City,
-            CountryCode = customer.CountryCode,
-            Country = customer.Country,
-            Emailaddress = customer.Emailaddress,
-            Zipcode = customer.Zipcode,
-            Streetaddress = customer.Streetaddress,
-            NationalId = customer.NationalId,
-            Telephonecountrycode = customer.Telephonecountrycode,
-            Telephonenumber = customer.Telephonenumber
-        };
+            Input = JsonSerializer.Deserialize<CustomerInputModel>(TempInputJson);
+        }
+        else
+        {
+            var customer = _commandService.GetCustomer(id);
+            if (customer == null) return NotFound();
+
+            Input = new CustomerInputModel
+            {
+                CustomerId = customer.CustomerId,
+                Givenname = customer.Givenname,
+                Surname = customer.Surname,
+                Gender = customer.Gender,
+                Birthday = customer.Birthday,
+                City = customer.City,
+                CountryCode = customer.CountryCode,
+                Country = customer.Country,
+                Emailaddress = customer.Emailaddress,
+                Zipcode = customer.Zipcode,
+                Streetaddress = customer.Streetaddress,
+                NationalId = customer.NationalId,
+                Telephonecountrycode = customer.Telephonecountrycode,
+                Telephonenumber = customer.Telephonenumber
+            };
+        }
+
+        if (!string.IsNullOrEmpty(TempModelStateJson))
+        {
+            var errors = JsonSerializer.Deserialize<Dictionary<string, string[]>>(TempModelStateJson);
+            if (errors != null)
+            {
+                foreach (var kvp in errors)
+                {
+                    foreach (var error in kvp.Value)
+                    {
+                        ModelState.AddModelError(kvp.Key, error);
+                    }
+                }
+            }
+        }
 
         return Page();
     }
@@ -49,11 +84,22 @@ public class UpdateModel : PageModel
     public IActionResult OnPost()
     {
         if (!ModelState.IsValid)
-            return Page();
+        {
+            TempInputJson = JsonSerializer.Serialize(Input);
+
+            var errors = ModelState
+                .Where(e => e.Value?.Errors.Count > 0)
+                .ToDictionary(
+                    kvp => kvp.Key,
+                    kvp => kvp.Value?.Errors.Select(e => e.ErrorMessage).ToArray() ?? new string[0]);
+
+            TempModelStateJson = JsonSerializer.Serialize(errors);
+
+            return RedirectToPage(new { id = Input.CustomerId });
+        }
 
         var customer = _commandService.GetCustomer(Input.CustomerId);
         if (customer == null) return NotFound();
-
 
         customer.Givenname = Input.Givenname;
         customer.Surname = Input.Surname;
--- Services/ViewModels/CustomerInputModel.cs
+++ Services/ViewModels/CustomerInputModel.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Services.ViewModels
 {
@@ -11,42 +8,57 @@ namespace Services.ViewModels
     {
         public int CustomerId { get; set; }
 
-        [Required, MaxLength(100)]
+        [Required(ErrorMessage = "First name is required")]
+        [MaxLength(100, ErrorMessage = "First name cannot exceed 100 characters")]
         public string Givenname { get; set; }
 
-        [Required, MaxLength(100)]
+        [Required(ErrorMessage = "Surname  is required")]
+        [MaxLength(100, ErrorMessage = "Surname cannot exceed 100 characters")]
         public string Surname { get; set; }
 
+        [Required(ErrorMessage = "Gender is required")]
         [MaxLength(100)]
         public string Gender { get; set; }
 
+        [Required(ErrorMessage = "Street address is required")]
         [MaxLength(100)]
         public string Streetaddress { get; set; }
 
+        [Required(ErrorMessage = "Zip code is required")]
         [MaxLength(10)]
         public string Zipcode { get; set; }
 
+        [Required(ErrorMessage = "Country code is required")]
         [MaxLength(2)]
         public string CountryCode { get; set; }
 
+        [Required(ErrorMessage = "Country is required")]
         [MaxLength(100)]
         public string Country { get; set; }
 
-        [DataType(DataType.Date)]
+        [Required(ErrorMessage = "Birthday is required")]
+        [DataType(DataType.Date, ErrorMessage = "Invalid date format")]
         public DateOnly? Birthday { get; set; }
 
-        [Required, MaxLength(50)]
+        [Required(ErrorMessage = "City is required")]
+        [MaxLength(50)]
         public string City { get; set; }
 
-        [EmailAddress, MaxLength(150)]
+        [Required(ErrorMessage = "Email address is required")]
+        [EmailAddress(ErrorMessage = "Invalid email address")]
+        [MaxLength(150)]
         public string Emailaddress { get; set; }
 
-        [MaxLength(20)]
+        [Required(ErrorMessage = "National ID is required")]
+        [RegularExpression(@"^\d+$", ErrorMessage = "National ID must contain only digits")]
+        [MaxLength(20, ErrorMessage = "National ID cannot exceed 20 digits")]
         public string NationalId { get; set; }
 
+        [Required(ErrorMessage = "Telephone country code is required")]
         [MaxLength(10)]
         public string Telephonecountrycode { get; set; }
 
+        [Required(ErrorMessage = "Telephone number is required")]
         [MaxLength(30)]
         public string Telephonenumber { get; set; }
     }
